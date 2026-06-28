4daaef9788266deb8a306e3cbfd8b3ae7ad44c26
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-06-28 09:06:41 +0300

    Update members.js

--- js/members.js
+++ js/members.js
@@ -1,1 +1,27 @@
+const modal = document.getElementById("memberModal");
 
+const openBtn = document.querySelector(".add-btn");
+
+const closeBtn = document.querySelector(".close");
+
+openBtn.onclick = () => {
+
+modal.style.display = "flex";
+
+}
+
+closeBtn.onclick = () => {
+
+modal.style.display = "none";
+
+}
+
+window.onclick = (e)=>{
+
+if(e.target==modal){
+
+modal.style.display="none";
+
+}
+
+}
